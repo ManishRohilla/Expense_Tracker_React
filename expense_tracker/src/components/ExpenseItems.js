d89e55698d8367204d1490c6d2b830ce9ce60db9
Manish Rohilla <kotamanishrohilla@gmail.com>
2024-02-20 10:27:54 +0530

    Expense Tracker React App Javascript variable in JSX

--- expense_tracker/src/components/ExpenseItems.js
+++ expense_tracker/src/components/ExpenseItems.js
@@ -1,14 +1,22 @@
 import React from "react";
+import "./ExpenseItems.css";
 
+const date = "22-Feb-2024";
+const expenseDetails = "Car Insurance";
+const LocationOfExpenditure = "Bengaluru";
+const expensePrice = 224.5;
 const ExpenseItems = () => {
   return (
     <div>
-      <h2>ExpenseItems</h2>
-      <p>Food Rs 10</p>
-
-      <p>Petrol Rs 100</p>
-
-      <p>Movies Rs 200</p>
+      <h2>Expenses</h2>
+      <div className="expense-item">
+        <div>{date}</div>
+        <div className="expense-item__description">
+          <h2>{expenseDetails}</h2>
+          <h2>{LocationOfExpenditure}</h2>
+          <div className="expense-item__price">${expensePrice}</div>
+        </div>
+      </div>
     </div>
   );
 };
