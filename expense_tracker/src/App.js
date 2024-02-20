a173426c9cc137b92fee673eb5a83879baeb1082
Manish Rohilla <kotamanishrohilla@gmail.com>
2024-02-20 11:54:19 +0530

    New Component for Expense Details

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -7,19 +7,19 @@ function App() {
       title: "Car Insurance",
       location: "Bengaluru",
       amount: 214.44,
-      date: new Date(22, 4, 2021),
+      date: new Date(2022, 1, 22),
     },
     {
       title: "Hotel",
       location: "Kolkata",
       amount: 300.67,
-      date: new Date(22, 2, 2024),
+      date: new Date(2021, 2, 20),
     },
     {
       title: "Flight Tickets",
       location: "Bengaluru",
       amount: 100.45,
-      date: new Date(29, 2, 2024),
+      date: new Date(2024, 2, 29),
     },
   ];
   const expenseItems = [];
---
+++ expense_tracker/src/components/ExpenseDetails.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ExpenseDetails = (props) => {
+  return (
+    <div className="expense-item__description">
+      <h2>{props.title}</h2>
+      <h2>{props.location}</h2>
+      <div className="expense-item__price">${props.price}</div>
+    </div>
+  );
+};
+
+export default ExpenseDetails;
