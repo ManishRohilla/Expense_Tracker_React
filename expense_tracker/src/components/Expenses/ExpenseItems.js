62e29e927abd4e2e6d50903d3dbdbac64d9948b0
Manish Rohilla <kotamanishrohilla@gmail.com>
2024-02-21 21:36:28 +0530

    Price Change Button -- useState

--- expense_tracker/src/components/Expenses/ExpenseItems.js
+++ expense_tracker/src/components/Expenses/ExpenseItems.js
@@ -1,27 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "./ExpenseItems.css";
 import ExpenseDetails from "./ExpenseDetails";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 const ExpenseItems = (props) => {
   const deleteExpense = () => {
-    // Delete expense logic here
-    // For demonstration, let's remove the expense item from the DOM
     const expenseItem = document.getElementById(`expense-${props.id}`);
     if (expenseItem) {
       expenseItem.remove();
     }
   };
+  const [title, setTitle] = useState(props.title);
+  const updateTitle = () => {
+    const titleNew = "Updated!";
+    setTitle(titleNew);
+    console.log(title);
+  };
+  const [price, setPrice] = useState(props.amount);
+  const updatePrice = () => {
+    const priceNew = "100";
+    setPrice(priceNew);
+    console.log(price);
+  };
   return (
     <Card className="expense-item" id={`expense-${props.id}`}>
       <ExpenseDate date={props.date} />
-      <ExpenseDetails
-        title={props.title}
-        location={props.location}
-        price={props.amount}
-      />
+      <ExpenseDetails title={title} location={props.location} price={price} />
       <button onClick={deleteExpense} className="delete-button">
         Delete
+      </button>
+      <button onClick={updateTitle} className="title-button">
+        Change Title
+      </button>
+      <button onClick={updatePrice} className="price-button">
+        Change Price
       </button>
     </Card>
   );
