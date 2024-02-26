1be81b757d2ac94e117de8a12f69b08aa3bb40ec
Manish Rohilla <kotamanishrohilla@gmail.com>
2024-02-26 12:22:47 +0530

    Delete Unnecessary Files

--- expense_tracker/src/App.js
+++ expense_tracker/src/App.js
@@ -3,30 +3,38 @@ import "./App.css";
 import React, { useState } from "react";
 import NewExpense from "./components/NewExpenses/NewExpense";
 
+const Dummy_Data = [
+  {
+    id: 1,
+    title: "Car Insurance",
+    location: "Bengaluru",
+    amount: 214.44,
+    date: new Date(2022, 1, 22),
+  },
+  {
+    id: 2,
+    title: "Hotel",
+    location: "Kolkata",
+    amount: 300.67,
+    date: new Date(2021, 2, 20),
+  },
+  {
+    id: 3,
+    title: "Flight Tickets",
+    location: "Bengaluru",
+    amount: 100.45,
+    date: new Date(2024, 2, 29),
+  },
+  {
+    id: 4,
+    title: "Bike",
+    location: "Bengaluru",
+    amount: 500.45,
+    date: new Date(2024, 3, 29),
+  },
+];
 function App() {
-  const [expenses, setExpense] = useState([
-    {
-      id: 1,
-      title: "Car Insurance",
-      location: "Bengaluru",
-      amount: 214.44,
-      date: new Date(2022, 1, 22),
-    },
-    {
-      id: 2,
-      title: "Hotel",
-      location: "Kolkata",
-      amount: 300.67,
-      date: new Date(2021, 2, 20),
-    },
-    {
-      id: 3,
-      title: "Flight Tickets",
-      location: "Bengaluru",
-      amount: 100.45,
-      date: new Date(2024, 2, 29),
-    },
-  ]);
+  let [expenses, setExpense] = useState(Dummy_Data);
   const sendForLog = (newExpense) => {
     newExpense = {
       ...newExpense,
